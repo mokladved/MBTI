1c885ad4ca6cae76a311b747e7549ca75e55627f
moklad <dev.moklad@gmail.com>
2025-08-13 17:38:03 +0900

    feat: 구조적인 UI 구성을 위한 UIConfigurable 프로토콜 생성

---
+++ MBTI/Protocols/UIConfigurable.swift
@@ -0,0 +1,14 @@
+//
+//  UIConfigurable.swift
+//  MBTI
+//
+//  Created by Youngjun Kim on 8/13/25.
+//
+
+import Foundation
+
+protocol UIConfigurable {
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+}
