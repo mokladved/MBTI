23438e9a677e8c4942d1227ac82de88d100b8076
moklad <dev.moklad@gmail.com>
2025-08-13 17:46:07 +0900

    feat: 코드 중복 제거와 일관된 구조를 위해 UIConfigurable을 채택한 BaseViewController 생성

---
+++ MBTI/Base/BaseViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BaseViewController.swift
+//  MBTI
+//
+//  Created by Youngjun Kim on 8/13/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController, UIConfigurable {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        view.backgroundColor = .
+    }
+
+}
+
