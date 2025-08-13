57e282c34ed6cbc4140fdbed1be3cdb937b9910b
moklad <dev.moklad@gmail.com>
2025-08-13 17:25:32 +0900

    feat: Constants enum을 통해 상수 관리

---
+++ MBTI/Enums/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  MBTI
+//
+//  Created by Youngjun Kim on 8/13/25.
+//
+
+import Foundation
+
+enum Constants {
+    enum Messages {
+        enum Error {
+            static let containNumber = "닉네임에 숫자는 포함할 수 없어요."
+            static let containSpecialCharacter = "닉네임에 @, #, $, %는 포함할 수 없어요"
+            static let invalidLength = "2글자 이상 10글자 미만으로 설정해주세요"
+        }
+        enum Success {
+            static let nickname = "사용할 수 있는 닉네임이에요"
+        }
+    }
+    
+    enum UI {
+        enum Title {
+            static let profileSetting = "PROFILE SETTING"
+        }
+        enum Placeholder {
+            static let profileSeting = "닉네임을 입력해주세요 :)"
+        }
+    }
+    
+}
