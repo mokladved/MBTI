76f77c537905cbab4d41cdbb001ac1820feb2185
moklad <dev.moklad@gmail.com>
2025-08-14 15:43:48 +0900

    feat: 재사용 가능한 커스텀 버튼 스타일을 UIButton Extension으로 추가

---
+++ MBTI/Extensions/UIButton+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIButton+Extension.swift
+//  MBTI
+//
+//  Created by Youngjun Kim on 8/14/25.
+//
+
+import UIKit
+
+extension UIButton.Configuration {
+    static func dalkomStyle(
+        title: String,
+        fontSize: CGFloat = 15,
+        bgColor: UIColor = .gray,
+        fgColor: UIColor = .black)
+    -> UIButton.Configuration {
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = bgColor
+        config.baseForegroundColor = fgColor
+        config.cornerStyle = .capsule
+        
+        var attributedTitle = AttributedString(title)
+        attributedTitle.font = .systemFont(ofSize: fontSize)
+        config.attributedTitle = attributedTitle
+        
+        return config
+    }
+}
