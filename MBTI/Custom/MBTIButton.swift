4b0971e153dd1b6dac95d85ba5da5fd3ef447a39
moklad <dev.moklad@gmail.com>
2025-08-16 14:56:56 +0900

    feat: 버튼 재활용을 위한 커스텀 버튼 추가

---
+++ MBTI/Custom/MBTIButton.swift
@@ -0,0 +1,51 @@
+//
+//  MBTIButton.swift
+//  MBTI
+//
+//  Created by Youngjun Kim on 8/16/25.
+//
+
+import UIKit
+
+final class MBTIButton: UIButton {
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        setTitle(title, for: .normal)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            updateUI()
+        }
+    }
+
+    private func configureUI() {
+        titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        layer.borderWidth = 2
+        updateUI()
+    }
+    
+    private func updateUI() {
+        if isSelected {
+            backgroundColor = .dalkomBlue
+            setTitleColor(.white, for: .normal)
+            layer.borderColor = UIColor.dalkomBlue.cgColor
+        } else {
+            backgroundColor = .white
+            setTitleColor(.black, for: .normal)
+            layer.borderColor = UIColor.lightGray.cgColor
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height / 2
+        clipsToBounds = true
+    }
+}
---
+++ MBTI/Custom/UnderlineTextField.swift
@@ -0,0 +1,49 @@
+//
+//  UnderlineTextField.swift
+//  MBTI
+//
+//  Created by Youngjun Kim on 8/15/25.
+//
+import UIKit
+import SnapKit
+
+class UnderlineTextField: UITextField, UIConfigurable {
+    
+    let underlineView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray
+        return view
+    }()
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() {
+        addSubview(underlineView)
+    }
+    
+    func configureLayout() {
+        underlineView.snp.makeConstraints { make in
+            make.horizontalEdges.equalToSuperview().inset(12)
+            make.bottom.equalToSuperview().offset(8)
+            make.height.equalTo(1)
+        }
+    }
+    
+    func configureView() {
+        borderStyle = .none
+        textColor = .black
+    }
+    
+    
+}
